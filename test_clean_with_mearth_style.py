1ff708252eba5e583e957a8b297e001e347051b4
Juliana Garcia-Mejia <juliana.garcia-mejia@cfa.harvard.edu>
2023-12-28 13:25:27 -0500

    test_clean_with_mearth_style.py: debugged call to make_global_lists function

--- test_clean_with_mearth_style.py
+++ test_clean_with_mearth_style.py
@@ -26,19 +26,19 @@ ap.add_argument("-target", required=True, help="Name of observed target exactly 
 ap.add_argument("-ffname", required=True, help="Name of folder in which to store reduced+flattened data. Convention is flatXXXX. XXXX=0000 means no flat was used.")
 ap.add_argument("-exclude_dates", nargs='*',type=str,help="Dates to exclude, if any. Write the dates separated by a space (e.g., 19950119 19901023)")
 ap.add_argument("-exclude_comps", required=False, nargs='*',type=int,help="Comparison stars to exclude, if any. Write the comp/ref number assignments ONLY, separated by a space (e.g., 2 5 7 if you want to exclude references/comps R2,R5, and R7.) ")
-ap.add_argument("-comp_weight_thold", required=False, type=float, default=0.01, help="Sets the weight threshold below which to ignore comparison stars. The weight of each comp star is read from the night_weights.csv folder for the FIRST night in the data set.")
+ap.add_argument("-comp_weight_thold", required=False, type=float, default=0.01, help="Sets the weight threshold below which to ignore comparison stars. The weight of each comp star is read from the night_weights.csv folder for the FIRST night in the data set.") #this default of 0.01 is arbitrary
 args = ap.parse_args()
 
 target = args.target
 ffname = args.ffname
 exclude_dates = np.array(args.exclude_dates)
-exclude_comps = np.array(args.exclude_dates)
+exclude_comps = np.array(args.exclude_comps)
 treshold = args.comp_weight_thold
 
 # Define data path
 basepath = '/data/tierras/'
 lcpath = os.path.join(basepath,'lightcurves')
-lcfolderlist = np.sort(glob.glob(lcpath+"/**/"+target))
+#lcfolderlist = np.sort(glob.glob(lcpath+"/**/"+target))
 
 # Load the list of comparison stars to use from the FIRST night in the list.
 compfname = os.path.join(lcfolderlist[0],ffname,"night_weights.csv")
@@ -57,7 +57,7 @@ complist = complist[mask & mask2]
 pdb.set_trace()
 
 # Load raw target and ref fluxes into global lists
-full_bjd, bjd_save, full_flux, full_err, full_reg = ld.make_global_lists(lcfolderlist)
+full_bjd, bjd_save, full_flux, full_err, full_reg = ld.make_global_lists(lcpath,target,ffname,exclude_dates)
 
 # mask bad data and use comps to calculate frame-by-frame magnitude zero points
 x, y, err = mearth_style(full_bjd, full_flux, full_err, full_reg) #TO DO: how to integrate weights into mearth_style?
