ddf7778939273e92e2ec02c7df02645dce2db370
patricktamburo <46655539+patricktamburo@users.noreply.github.com>
2024-01-08 15:39:14 -0500

    iterative prot pipeline

--- corrected_flux_plot.py
+++ corrected_flux_plot.py
@@ -6,15 +6,16 @@ import matplotlib.pyplot as plt
 plt.ion()
 import copy 
 
-def corrected_flux_plot(bjds, regressors, cs):
+def reference_flux_correction(bjds, regressors, cs, complist, plot=False):
     regressors_adj = np.zeros_like(regressors) #Adjust the regressor fluxes by cs
     regressors_adj_norm = np.zeros_like(regressors)
     for ii in range(regressors.shape[0]):
         regressors_adj[ii,:] = regressors[ii,:]*10**(cs/(-2.5))
-        regressors_adj_norm[ii,:] = regressors_adj[ii,:] / np.nanmean(regressors_adj[ii,:])
-
+        regressors_adj_norm[ii,:] = regressors_adj[ii,:] / np.nanmean(regressors_adj[ii,:]) #Create normalized adjusted fluxes
+    
+    #Figure out which indices correspond to which date by checking the spacing between exposure times
     time_deltas = np.array([bjds[i+1]-bjds[i] for i in range(len(bjds)-1)])
-    date_ends = bjds[np.where(time_deltas>0.8)[0]]
+    date_ends = bjds[np.where(time_deltas>0.5)[0]] #If the separation is > half a day, say that that is where the date ends
     date_inds = []
     for ii in range(len(date_ends)+1):
         if ii == 0:
@@ -23,39 +24,45 @@ def corrected_flux_plot(bjds, regressors, cs):
             date_inds.append(np.where((bjds > date_ends[ii-1]) & (bjds <= date_ends[ii]))[0])
         else:
             date_inds.append(np.where(bjds > date_ends[-1])[0])
-    colors=  ['tab:blue','tab:orange','tab:green','tab:red','tab:purple','tab:brown','tab:pink','tab:gray','tab:olive','tab:cyan']
-    markers = ['.','*']
-    binned_flux = np.zeros((regressors.shape[0], len(date_inds)))
-    fig, ax = plt.subplots(2, len(date_inds), figsize=(16,5), sharey='row', sharex=True)
-    random_offsets = np.random.uniform(-0.01, 0.01, regressors.shape[0])
-    #flux_norm = flux / np.nanmean(flux)
-    #flux_original_norm = flux_original / np.nanmean(flux_original)
-    for ii in range(regressors.shape[0]):
-        color = colors[ii%len(colors)]
-        if ii < 10:
-            marker = markers[0]
-        else:
-            marker = markers[1]
-        for jj in range(len(date_inds)):
-            plot_times = bjds[date_inds[jj]]
-            plot_times -= plot_times[0]
-            plot_times *= 24 
-            ax[0,jj].plot(plot_times, regressors[ii][date_inds[jj]]/np.mean(regressors[ii]), marker='.', ls='' ,color=color, alpha=1, mec='none', zorder=0)
-            #ax[0,jj].plot(bjds[date_inds[jj]], 10**(cs[date_inds[jj]]/(-2.5)), lw=2, color='k')
-            ax[1,jj].plot(plot_times, regressors_adj_norm[ii][date_inds[jj]], marker=marker, ls='' ,color=color, alpha=1, mec='none', zorder=0)
-            #if ii == 0:
-                #ax[0,jj].plot(plot_times,flux_original_norm[date_inds[jj]], marker=marker, color='k', ls='', zorder=3)
-                #ax[1,jj].plot(plot_times,flux_norm[date_inds[jj]], marker='.', color='k', ls='', zorder=3)
-            mean_bjd = np.mean(plot_times)
-            mean_flux = np.median(regressors_adj_norm[ii][date_inds[jj]])
-            binned_flux[ii, jj] = mean_flux
-            if jj == 0:
-                ax[1,jj].plot(mean_bjd+random_offsets[ii], mean_flux, marker=marker,color=color, mec='k', mew=1.5, zorder=1,label=f'Ref {ii+1}', ls='', ms=10)
-            else:
-                ax[1,jj].plot(mean_bjd+random_offsets[ii], mean_flux, marker=marker,color=color, mec='k', mew=1.5, zorder=1, ls='', ms=10)
 
-        #plt.plot(bjds, flux/np.mean(flux), marker='.', ls='', label='Target')
-    ax[1,jj].legend()
-    plt.tight_layout()
-    plt.subplots_adjust(hspace=0,wspace=0)
-    return fig, ax, binned_flux
+    #Calculate mean fluxes for each star on each night 
+        
+    binned_flux = np.zeros((regressors.shape[0], len(date_inds)))
+    for ii in range(regressors.shape[0]):
+        for jj in range(len(date_inds)):
+            binned_flux[ii,jj] = np.median(regressors_adj_norm[ii][date_inds[jj]])
+    if not plot:
+        return binned_flux
+    else:
+        #Plot uncorrected/corrected reference stars
+        colors=  ['tab:blue','tab:orange','tab:green','tab:red','tab:purple','tab:brown','tab:pink','tab:gray','tab:olive','tab:cyan']
+        markers = ['.','*','s','v','^','p','P']
+        fig, ax = plt.subplots(2, len(date_inds), figsize=(16,5), sharey='row', sharex=True)
+        random_offsets = np.random.uniform(-0.5, 0.5, regressors.shape[0])
+        #flux_norm = flux / np.nanmean(flux)
+        #flux_original_norm = flux_original / np.nanmean(flux_original)
+        marker_ind = 0
+        for ii in range(regressors.shape[0]):
+            color = colors[ii%len(colors)]
+            if (ii > 0) and (ii%len(colors)) == 0:
+                marker_ind += 1 
+            marker = markers[marker_ind]
+
+            for jj in range(len(date_inds)):
+                plot_times = bjds[date_inds[jj]]
+                plot_times -= plot_times[0]
+                plot_times *= 24 
+
+                ax[0,jj].plot(plot_times, regressors[ii][date_inds[jj]]/np.mean(regressors[ii]), marker='.', ls='' ,color=color, alpha=0.5, mec='none', zorder=0)
+                ax[1,jj].plot(plot_times, regressors_adj_norm[ii][date_inds[jj]], marker=marker, ls='' ,color=color, alpha=0.5, mec='none', zorder=0)
+                mean_bjd = np.mean(plot_times)
+                if jj == len(date_inds)-1:
+                    ax[1,jj].plot(mean_bjd+random_offsets[ii], binned_flux[ii,jj], marker=marker,color=color, mec='k', mew=1.5, zorder=1,label=f'Ref {complist[ii]}', ls='', ms=10)
+                else:
+                    ax[1,jj].plot(mean_bjd+random_offsets[ii], binned_flux[ii,jj], marker=marker,color=color, mec='k', mew=1.5, zorder=1, ls='', ms=10)
+
+            #plt.plot(bjds, flux/np.mean(flux), marker='.', ls='', label='Target')
+        ax[1,-1].legend(loc='center left', bbox_to_anchor=(1, 1), ncol=2)
+        plt.tight_layout()
+        plt.subplots_adjust(hspace=0,wspace=0)
+        return fig, ax, binned_flux
---
+++ noise_calculation.py
@@ -0,0 +1,101 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+plt.ion()
+import glob 
+import pandas as pd
+from scipy.stats import sigmaclip
+import os 
+
+def noise_component_plot(ap_rad=10, exp_time=30, sky=100):
+    conversion = 1e6 # 1e3 for ppt, 1e6 for ppm 
+    
+    n_pix = np.pi*ap_rad**2
+    READ_NOISE = 18.5
+    DARK_CURRENT = 0.19
+
+    source_photon_counts = np.logspace(np.log10(1e4),np.log10(3e7),1000)
+
+    sky_photon_counts = sky*n_pix
+    source_photon_noise = np.sqrt(source_photon_counts) / source_photon_counts
+    sky_photon_noise = np.sqrt(sky_photon_counts) / source_photon_counts
+    read_noise = np.sqrt((np.zeros_like(source_photon_counts) + n_pix * READ_NOISE**2)) / source_photon_counts
+    dc_noise = np.sqrt((np.zeros_like(source_photon_counts) + n_pix * DARK_CURRENT * exp_time)) / source_photon_counts
+    pwv_noise = (np.zeros_like(source_photon_counts) + 250/1e6) / source_photon_counts
+
+    noise_floor = 250 / 1e6 * source_photon_counts / source_photon_counts
+
+    total_noise = (np.sqrt(source_photon_counts + sky_photon_counts  + n_pix*(DARK_CURRENT*exp_time + READ_NOISE**2)) + noise_floor*source_photon_counts)/ source_photon_counts
+    
+    fig, ax = plt.subplots(1,1,figsize=(10,7))
+    ax.plot(source_photon_counts, source_photon_noise*conversion, label='Source', ls='--')
+    ax.plot(source_photon_counts, sky_photon_noise*conversion, label=f'Sky ({sky}) phot./pix.', ls='--')
+    #ax.plot(source_photon_counts, read_noise*conversion, label='Read noise')
+    #ax.plot(source_photon_counts, dc_noise*conversion, label='Dark current')
+    ax.plot(source_photon_counts, noise_floor*conversion, label='Noise floor', ls='--')
+    ax.plot(source_photon_counts, total_noise*conversion, label='Total noise', lw=2)
+
+    ax.legend()
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.invert_xaxis()
+    ax.tick_params(labelsize=14)
+    ax.set_xlabel('Source counts (photons)', fontsize=16)
+    if conversion == 1e3:
+        ax.set_ylabel('$\sigma$ (ppt)', fontsize=16)
+    elif conversion == 1e6:
+        ax.set_ylabel('$\sigma$ (ppm)', fontsize=16)
+    else:
+        ax.set_ylabel('$\sigma$', fontsize=16)
+    ax.set_ylim(80,1e6)
+    ax.set_xlim(max(source_photon_counts),min(source_photon_counts))
+    plt.tight_layout()
+    return fig, ax 
+
+if __name__ == '__main__':
+    fig, ax = noise_component_plot()
+    targets = glob.glob('/data/tierras/targets/*')
+    for i in range(len(targets)):
+        target = targets[i].split('/')[-1]
+        dates = glob.glob(f'/data/tierras/lightcurves/**/{target}')
+        for j in range(len(dates)):
+            date = dates[j].split('/')[-2]
+            print(target, date)
+            try:
+                optimal_lc_path = f'/data/tierras/lightcurves/{date}/{target}/flat0000/optimal_lc.txt'
+                if os.path.exists(optimal_lc_path):
+                    with open(optimal_lc_path) as f:
+                        path = f.readline()
+                else:
+                    optimal_lc_path = f'/data/tierras/lightcurves/{date}/{target}/flat000/optimal_lc.txt'
+                    if os.path.exists(optimal_lc_path):
+                        with open(optimal_lc_path) as f:
+                            path = f.readline()
+                    else:
+                        print(f'No optimal output for {target} on {date}, skipping.')
+                        continue
+                df = pd.read_csv(path)
+            except:
+                continue
+            n_refs = int(df.keys()[-1].split(' ')[1])
+            target_rel_flux = np.array(df['Target Relative Flux'])
+            target_raw_flux = np.array(df['Target Source-Sky e'])
+            v, l, h = sigmaclip(target_rel_flux)
+            use_inds = np.where((target_rel_flux>l)&(target_rel_flux<h))[0]
+            target_rel_flux = target_rel_flux[use_inds]
+            target_raw_flux = target_raw_flux[use_inds]
+            norm = np.median(target_rel_flux)
+            target_rel_flux /= norm
+            stddev = np.nanstd(target_rel_flux)*1e6
+            ax.plot(np.mean(target_raw_flux), stddev, '.', color='k', ls='', alpha=0.4, ms=3)
+            for k in range(n_refs):
+                ref_rel_flux = np.array(df[f'Ref {k+1} Relative Flux'])
+                ref_raw_flux = np.array(df[f'Ref {k+1} Source-Sky e'])
+                v, l, h = sigmaclip(ref_rel_flux)
+                use_inds = np.where((ref_rel_flux>l)&(ref_rel_flux<h))[0]
+                ref_rel_flux = ref_rel_flux[use_inds]
+                ref_raw_flux = ref_raw_flux[use_inds]
+                norm = np.median(ref_rel_flux)
+                ref_rel_flux /= norm
+                stddev = np.nanstd(ref_rel_flux)*1e6
+                ax.plot(np.mean(ref_raw_flux), stddev, '.', color='k', ls='', alpha=0.4, ms=3)
+    breakpoint()
