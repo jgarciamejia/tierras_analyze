fixed uncertainty calculation; now sigma_ccd is calculated in fixed_circular_aperture_photometry, sigma_scint is calculated in the mearth_style program in tierras_analyze

corrected_flux_plot.py
@@ -9,7 +9,7 @@ from astropy.time import Time
 from ap_phot import regression 
 from scipy.stats import pearsonr 
 
-def reference_flux_correction(bjds, targ_flux, regressors, regressors_err, cs, cs_unc, complist, weights, airmass, fwhm, plot=False):
+def reference_flux_correction(bjd, bjd_list,  regressors, regressors_err, cs, cs_unc, complist, weights, airmass, fwhm):
     regressors_adj = np.zeros_like(regressors) #Adjust the regressor fluxes by cs
     regressors_adj_norm = np.zeros_like(regressors)
     regressors_adj_err = np.zeros_like(regressors)
@@ -28,74 +28,45 @@ def reference_flux_correction(bjds, targ_flux, regressors, regressors_err, cs, c
         # regressors_adj_norm[ii,:] = reg_flux
         # regressors_adj[ii,:] = reg_flux * norm
 
-    #Figure out which indises correspond to which date by checking the spacing between exposure times
-    time_deltas = np.array([bjds[i+1]-bjds[i] for i in range(len(bjds)-1)])
-    date_ends = bjds[np.where(time_deltas>0.5)[0]] #If the separation is > half a day, say that that is where the date ends
-    date_inds = []
-    for ii in range(len(date_ends)+1):
-        if ii == 0:
-            date_inds.append(np.where(bjds <= date_ends[ii])[0])
-        elif ii < len(date_ends):
-            date_inds.append(np.where((bjds > date_ends[ii-1]) & (bjds <= date_ends[ii]))[0])
-        else:
-            date_inds.append(np.where(bjds > date_ends[-1])[0])
 
-    #Calculate mean fluxes for each star on each night 
+    binned_flux = np.zeros((regressors.shape[0], len(bjd_list)))
+
+    fig, ax = plt.subplots(2, len(bjd_list), figsize=(16,5), sharey='row', sharex=True)
+    colors=  ['tab:blue','tab:orange','tab:green','tab:red','tab:purple','tab:brown','tab:pink','tab:gray','tab:olive','tab:cyan']
+    markers = ['.','*','s','v','^','p','P']
+    random_offsets = np.random.uniform(-0.5, 0.5, regressors.shape[0])
+    for ii in range(len(bjd_list)):
+        use_inds = np.where((bjd >= bjd_list[ii][0])&(bjd <= bjd_list[ii][-1]))[0]
+        plot_times = bjd[use_inds]
+        ut_date = Time(plot_times[-1],format='jd',scale='tdb').iso.split(' ')[0]
+        date_label = ut_date.split('-')[1].lstrip('0')+'/'+ut_date.split('-')[2].lstrip('0')+'/'+ut_date[2:4]
+        plot_times -= plot_times[0]
+        plot_times *= 24 
+        ax[0,ii].set_title(date_label, rotation=45, fontsize=8)
         
-    binned_flux = np.zeros((regressors.shape[0], len(date_inds)))
-    for ii in range(regressors.shape[0]):
-        for jj in range(len(date_inds)):
-            binned_flux[ii,jj] = np.median(regressors_adj_norm[ii][date_inds[jj]])
-    if not plot:
-        return binned_flux
-    else:
-        #Plot uncorrected/corrected reference stars
-        colors=  ['tab:blue','tab:orange','tab:green','tab:red','tab:purple','tab:brown','tab:pink','tab:gray','tab:olive','tab:cyan']
-        markers = ['.','*','s','v','^','p','P']
-        fig, ax = plt.subplots(2, len(date_inds), figsize=(16,5), sharey='row', sharex=True)
-        random_offsets = np.random.uniform(-0.5, 0.5, regressors.shape[0])
-        #flux_norm = flux / np.nanmean(flux)
-        #flux_original_norm = flux_original / np.nanmean(flux_original)
         marker_ind = 0
-        for ii in range(regressors.shape[0]):
-            if weights[ii] == 0:
-                continue 
-
-            color = colors[ii%len(colors)]
-            if (ii > 0) and (ii%len(colors)) == 0:
+        for jj in range(regressors.shape[0]):
+            color = colors[jj%len(colors)]
+            if (jj > 0) and (jj%len(colors)) == 0:
                 marker_ind += 1 
             marker = markers[marker_ind]
 
-            for jj in range(len(date_inds)):
-                plot_times = bjds[date_inds[jj]]
-                ut_date = Time(plot_times[-1],format='jd',scale='tdb').iso.split(' ')[0]
-                date_label = ut_date.split('-')[1].lstrip('0')+'/'+ut_date.split('-')[2].lstrip('0')+'/'+ut_date[2:4]
-                plot_times -= plot_times[0]
-                plot_times *= 24 
+            binned_flux[jj,ii] = np.median(regressors_adj_norm[jj][use_inds])
 
-                ax[0,jj].set_title(date_label, rotation=45, fontsize=8)
-                ax[0,jj].plot(plot_times, regressors[ii][date_inds[jj]]/np.mean(regressors[ii]), marker='.', ls='' ,color=color, alpha=0.5, mec='none', zorder=0)
-                ax[1,jj].plot(plot_times, regressors_adj_norm[ii][date_inds[jj]], marker=marker, ls='' ,color=color, alpha=0.5, mec='none', zorder=0)
-                mean_bjd = np.mean(plot_times)
-                if jj == len(date_inds)-1:
-                    ax[1,jj].plot(mean_bjd+random_offsets[ii], binned_flux[ii,jj], marker=marker,color=color, mec='k', mew=1.5, zorder=1,label=f'R{complist[ii]}, {100*weights[ii]:.1f}%', ls='', ms=10)
-                else:
-                    ax[1,jj].plot(mean_bjd+random_offsets[ii], binned_flux[ii,jj], marker=marker,color=color, mec='k', mew=1.5, zorder=1, ls='', ms=10)
+            if weights[jj] == 0:
+                continue 
 
-            #plt.plot(bjds, flux/np.mean(flux), marker='.', ls='', label='Target')
-        
-        # # Plot the corrected target_flux 
-        # targ_flux /= np.median(targ_flux)
-        # for jj in range(len(date_inds)):
-        #     plot_times = bjds[date_inds[jj]]
-        #     plot_times -= plot_times[0]
-        #     plot_times *= 24 
-        #     plot_times = np.mean(plot_times)
-        #     mean_flux = np.mean(targ_flux[date_inds[jj]])
-        #     ax[1,jj].plot(plot_times,mean_flux, marker='o', mfc='w', mec='k', mew=2, alpha=1, zorder=5, ms=8)
-        # breakpoint()
 
-        ax[1,-1].legend(loc='center left', bbox_to_anchor=(1, 1), ncol=2)
-        plt.tight_layout()
-        plt.subplots_adjust(left=0.04,right=0.8,hspace=0,wspace=0)
-        return fig, ax, binned_flux, regressors_adj_norm
+            ax[0,ii].plot(plot_times, regressors[jj][use_inds]/np.mean(regressors[jj]), marker='.', ls='' ,color=color, alpha=0.5, mec='none', zorder=0)
+            ax[1,ii].plot(plot_times, regressors_adj_norm[jj][use_inds], marker=marker, ls='' ,color=color, alpha=0.5, mec='none', zorder=0)
+            mean_bjd = np.mean(plot_times)
+            if ii == len(bjd_list)-1:
+                ax[1,ii].plot(mean_bjd+random_offsets[jj], binned_flux[jj,ii], marker=marker,color=color, mec='k', mew=1.5, zorder=1,label=f'R{complist[jj]}, {100*weights[jj]:.1f}%', ls='', ms=10)
+            else:
+                ax[1,ii].plot(mean_bjd+random_offsets[jj], binned_flux[jj,ii], marker=marker,color=color, mec='k', mew=1.5, zorder=1, ls='', ms=10)
+
+    ax[1,-1].legend(loc='center left', bbox_to_anchor=(1, 1), ncol=2)
+    plt.tight_layout()
+    plt.subplots_adjust(left=0.04,right=0.8,hspace=0,wspace=0)
+    
+    return fig, ax, binned_flux, regressors_adj_norm

test_prot_analysis_pipeline_pat.py
@@ -45,7 +45,7 @@ ap.add_argument('-restore', default=False, help='If True, the code will try to r
 ap.add_argument('-median_filter_w', default=0, help='The width of the median filter (in days) to be applied to the data set. If 0, no filtering will be done.')
 ap.add_argument('-regression', default=False, help='If True, the code will do regression on target flux on each night against target x/y pixel positions, average FWHM, and airmass.')
 ap.add_argument('-airmass_limit', required=False, default=2, help='Maximum airmass of a cadence that will be retained in the analysis.')
-ap.add_argument('-duration_limit', required=False, default=1, help='Minimum duration (in hours) that a night of observations must have to be retained in the analysis. Note that this limit refers to the total exposure time on a given night, the data do not have to be continuous.')
+ap.add_argument('-duration_limit', required=False, default=0.5, help='Minimum duration (in hours) that a night of observations must have to be retained in the analysis. Note that this limit refers to the total exposure time on a given night, the data do not have to be continuous.')
 ap.add_argument('-fwhm_limit', required=False, default=3.5, help='Maximum FWHM (in arcseconds) that a cadence must have to be retained in the analysis.')
 
 args = ap.parse_args()
@@ -125,9 +125,16 @@ full_reg_err_loop = copy.deepcopy(full_reg_err)[mask]
 # mask bad data and use comps to calculate frame-by-frame magnitude zero points
 x, y, err, masked_reg, masked_reg_err, cs, c_unc, exp_times, skies, weights, x_pos, y_pos, airmass, fwhm, bjd_save = mearth_style_pat_weighted(full_bjd, full_flux_div_expt, full_err_div_expt, full_reg_loop, full_reg_err_loop, full_exptime, full_sky, full_x_pos, full_y_pos, full_airmass, full_fwhm, bjd_save, duration_limit, airmass_limit, fwhm_limit) 
 
-# Generate the corrected flux figure
-resfig, resax, binned_fluxes, masked_reg_corr = reference_flux_correction(x, y, masked_reg, masked_reg_err, cs, c_unc, complist[mask], weights[mask], airmass, fwhm, plot=True) 
+plt.figure()
+plt.hist(airmass)
+plt.xlabel('Airmass')
 
+plt.figure()
+plt.hist(fwhm)
+plt.xlabel('FWHM')
+
+# Generate the corrected flux figure
+resfig, resax, binned_fluxes, masked_reg_corr = reference_flux_correction(x, bjd_save, masked_reg, masked_reg_err, cs, c_unc, complist[mask], weights[mask], airmass, fwhm) 
 
 reg_corr_stds = np.nanstd(masked_reg_corr,axis=1)[np.where(weights!=0)[0]]
 print(np.nanmedian(reg_corr_stds))
@@ -252,13 +259,16 @@ for i in range(len(masked_reg_corr)):
 # plt.tight_layout()
 
 plt.figure()
-for i in range(len(ref_dists)):
-    plt.scatter(G_mags[i], stddevs[i], color=colors[detector_half[i]])
+color_inds = np.zeros(len(weights),dtype='int')
+color_inds[np.where(weights==0)[0]] = 1
+plt.scatter(G_mags, stddevs, color=np.array(colors)[color_inds])
+plt.axvline(refdf['G'][0],color='tab:red')
 
 plt.xlabel('G mag', fontsize=16)
 plt.ylabel('$\sigma$ (ppt)', fontsize=16)
 plt.tick_params(labelsize=14)
 plt.tight_layout()
+breakpoint()
 
 # plt.figure()
 # for i in range(len(binned_fluxes)):